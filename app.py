c083a9c1d0cda98b10bbdb25365f8aa533b69d77
Ibrahim Osama <106915409+He-ema@users.noreply.github.com>
2026-09-22 08:23:26 +0300

    Update home route response message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, This is my CD Test!"
+    return "Hello, This is my App Test!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
