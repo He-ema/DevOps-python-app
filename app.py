e839ec2677b2b40cafa7b30211eded39e7383302
Ibrahim Osama <106915409+He-ema@users.noreply.github.com>
2026-09-22 22:03:26 +0300

    Update home route response message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello,This is my DevOps project test!"
+    return "Hello,Project workflow test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
